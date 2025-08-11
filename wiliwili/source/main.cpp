a3b19ea852df8d87c5604883446660796be12df4
ikas <65895428+ikas-mc@users.noreply.github.com>
2025-08-11 17:35:27 +0800

    enable new  winrt

--- wiliwili/source/main.cpp
+++ wiliwili/source/main.cpp
@@ -26,6 +26,11 @@
 #include <winrt/Windows.UI.Core.h>
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#endif
+
+#ifdef __WINRT_NEW__
+#include <borealis/platforms/winrt/winrt_app.hpp>
 #endif
 
 int main(int argc, char* argv[]) {
@@ -158,6 +163,12 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
-#ifdef __WINRT__
+#ifdef __WINRT_NEW__
+int __stdcall wWinMain (HINSTANCE, HINSTANCE, PWSTR szCmdLine, int)
+{
+    (void)szCmdLine;
+    return WinrtApp::RunApp (main);
+}
+#elif __WINRT__
 #include <borealis/core/main.hpp>
 #endif
